97b672d4ece5a4d7d740bc60bbd343f6c99299e8
Rameez Ul Hassan <ulhassanr59@gmail.com>
2023-12-03 22:27:31 +0500

    components

---
+++ Next JS Programs/3-components/app/about/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Navbar from '../components/navbar/Navbar'
+
+export default function About() {
+  return (
+    <main>
+    <Navbar/>
+    <div>This is About page</div>
+    </main>
+  )
+}
---
+++ Next JS Programs/3-components/app/page.tsx
@@ -0,0 +1,11 @@
+import Image from 'next/image'
+import Navbar from './components/navbar/Navbar'
+
+export default function Home() {
+  return (
+    <main>
+    <Navbar/>
+    <div>This is home page</div>
+    </main>
+  )
+}
