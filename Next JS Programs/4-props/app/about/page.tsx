da43b3b9509221af552ef075af8296f1b48183f8
Rameez Ul Hassan <ulhassanr59@gmail.com>
2023-12-10 22:44:26 +0500

    learn props

--- Next JS Programs/4-props/app/about/page.tsx
+++ Next JS Programs/4-props/app/about/page.tsx
@@ -5,7 +5,7 @@ import { Navbar } from '../components/navbar/Navbar'
 export default function About() {
   return (
     <main>
-      <Navbar nav1="Home" nav2="About"  nav3="services"/>
+      <Navbar nav1="Home" nav2="About"  nav3="services" nav4='Book Now'/>
     </main>
   )
 }
